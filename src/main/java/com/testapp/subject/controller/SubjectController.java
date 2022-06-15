53d0f16d38d59c23d191a9f91fa321589773bb90
shamsiddin18 <83861595+shamsiddin18@users.noreply.github.com>
2022-06-15 14:38:19 +0500

    question edit (#10)

--- src/main/java/com/testapp/subject/controller/SubjectController.java
+++ src/main/java/com/testapp/subject/controller/SubjectController.java
@@ -57,7 +57,7 @@ public class SubjectController {
         model.addAttribute("questions", questions);
         model.addAttribute("answers", answersMap);
 
-        return  "question/list";
+        return  "question/view";
     }
 
     @PostMapping("/testing")
