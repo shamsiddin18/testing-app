4fdc319a08e1b4716abdacf3ee4d1cec9bc28344
Zafar Buribaev <uzproger@gmail.com>
2022-07-04 16:53:10 +0500

    Question service unit test (#18)
    * testing for answer/question/testing/subject
    
    * testing answer/question/subject/testing
    
    Co-authored-by: Shamsiddin <shamsiddin042@mail.ru>

--- src/main/java/com/testapp/user/service/UserService.java
+++ src/main/java/com/testapp/user/service/UserService.java
@@ -30,7 +30,6 @@ public class UserService  implements UserDetailsService {
         if(user == null){
             throw new UsernameNotFoundException("User is not found");
         }
-
         return user;
     }
 }
---
+++ src/test/java/com/testapp/answer/AnswerServiceTest.java
@@ -0,0 +1,22 @@
+package com.testapp.answer;
+
+import com.testapp.answer.model.Answer;
+import com.testapp.answer.repository.AnswerRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswerServiceTest {
+
+    @Test
+   public void save_test_answer(){
+        AnswerRepository answerRepository = Mockito.mock(AnswerRepository.class);
+        Answer answer= new Answer();
+        Mockito.when(answerRepository.save(answer)).thenReturn(answer);
+        AnswerService answerService = new AnswerService(answerRepository);
+        Answer result = answerService.create(answer);
+        assertEquals(result, answer);
+        Mockito.verify(answerRepository).save(answer);
+    }
+}
