8df9593432d7be898feb39806dd6ed5fd63eb77a
Shamsiddin <shamsiddin042@mail.ru>
2022-07-07 13:42:48 +0500

    test case for subject validation

---
+++ src/test/java/com/testapp/subject/constraint/UniqueTitleValidatorTest.java
@@ -0,0 +1,55 @@
+package com.testapp.subject.constraint;
+
+import com.testapp.subject.model.Subject;
+import com.testapp.subject.repository.SubjectRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueTitleValidatorTest {
+
+    @Test
+    public void when_title_is_empty_it_should_be_valid() {
+        SubjectRepository subjectRepository = Mockito.mock(SubjectRepository.class);
+        ConstraintValidatorContext constraintValidator = Mockito.mock(ConstraintValidatorContext.class);
+        UniqueTitleValidator uniqueTitleValidator = new UniqueTitleValidator(subjectRepository);
+        assertTrue(uniqueTitleValidator.isValid("", constraintValidator));
+    }
+
+    @Test
+    public void when_subject_repository_return_null_it_should_be_valid(){
+        SubjectRepository subjectRepository = Mockito.mock(SubjectRepository.class);
+        ConstraintValidatorContext constraintValidatorContext = Mockito.mock(ConstraintValidatorContext.class);
+        Subject subject = new Subject();
+        subject.setTitle("math");
+
+        Mockito.when(subjectRepository.findFirstByTitle("math")).thenReturn(Optional.empty());
+        UniqueTitleValidator uniqueTitleValidator = new UniqueTitleValidator(subjectRepository);
+        boolean result = uniqueTitleValidator.isValid(subject.getTitle(), constraintValidatorContext);
+        assertTrue(result);
+        Mockito.verify(subjectRepository).findFirstByTitle("math");
+
+    }
+
+    @Test
+    public void when_repository_return_not_null_it_should_be_false(){
+        SubjectRepository subjectRepository = Mockito.mock(SubjectRepository.class);
+        ConstraintValidatorContext constraintValidatorContext = Mockito.mock(ConstraintValidatorContext.class);
+        Subject subject = new Subject();
+        subject.setTitle("math");
+
+        Mockito.when(subjectRepository.findFirstByTitle("math")).thenReturn(Optional.of(subject));
+        UniqueTitleValidator uniqueTitleValidator = new UniqueTitleValidator(subjectRepository);
+        boolean result = uniqueTitleValidator.isValid(subject.getTitle(), constraintValidatorContext);
+        assertFalse(result);
+        Mockito.verify(subjectRepository).findFirstByTitle("math");
+    }
+
+
+}
