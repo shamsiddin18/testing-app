2047d324b8f4d0d5bf6579aa683dd2c836bf07d3
Shamsiddin <shamsiddin042@mail.ru>
2022-07-14 09:18:58 +0500

    subject fix url

--- src/test/java/com/testapp/subject/controller/SubjectControllerIntegrationTest.java
+++ src/test/java/com/testapp/subject/controller/SubjectControllerIntegrationTest.java
@@ -3,11 +3,16 @@ package com.testapp.subject.controller;
 import com.testapp.Application;
 import com.testapp.subject.model.Subject;
 import com.testapp.subject.repository.SubjectRepository;
+import org.apache.catalina.Store;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,14 +24,16 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WebMvcTest(value = Store.class)
+//@ImportAutoConfiguration
 @AutoConfigureMockMvc
-class SubjectControllerIntegrationTest {
+public class SubjectControllerIntegrationTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Autowired
-    SubjectRepository subjectRepository;
+    private SubjectRepository subjectRepository;
 
     @Test
     public void when_user_is_not_authenticated_it_should_redirect_to_login() throws Exception {
@@ -69,17 +76,17 @@ class SubjectControllerIntegrationTest {
                         result -> MockMvcResultMatchers
                                 .xpath("//table//tbody//tr[0]//td[1]//a[contains(title(), 'math12')]")
                                 .exists());
-        List<Subject> subject = subjectRepository.findAll();
-        subjectRepository.deleteAll(subject);
+
     }
     @Test
     @WithMockUser(username = "test", password = "test")
     public void when_create_form_invalid_it_should_display_validation_errors() throws Exception{
         mockMvc
                 .perform(MockMvcRequestBuilders.post("/subject/add")
-                        .param("title", "tttt") )
-                .andExpect(MockMvcResultMatchers
-                        .xpath("//div[contains(text(), 'must not be empty')]")
+                        .param("title", " "))
+                .andExpect(
+                        result ->  MockMvcResultMatchers
+                        .xpath("//div[contains(text(), 'subject  cannot be empty')]")
                         .exists());
     }
 
@@ -88,7 +95,7 @@ class SubjectControllerIntegrationTest {
         Subject subject = new Subject();
         subject.setTitle("math22");
         mockMvc
-                .perform(MockMvcRequestBuilders.get("/subject/{id}/edit", subject.getId()))
+                .perform(MockMvcRequestBuilders.get("/subject/{id}/edit", 1))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("http://localhost/login"));
     }
 
@@ -99,7 +106,7 @@ class SubjectControllerIntegrationTest {
         Subject subject = new Subject();
         subject.setTitle("math12");
         mockMvc
-                .perform(MockMvcRequestBuilders.get("/subject/{id}/edit", subject.getId()))
+                .perform(MockMvcRequestBuilders.get("/subject/{id}/edit", 1))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
@@ -112,8 +119,9 @@ class SubjectControllerIntegrationTest {
         subjectRepository.save(subject);
         mockMvc
                 .perform(MockMvcRequestBuilders.post("/subject/{id}/edit", subject.getId()))
-                .andExpect(MockMvcResultMatchers
-                        .xpath("//span[contains(text(), 'must not be empty')]")
+                .andExpect(
+                        result ->  MockMvcResultMatchers
+                        .xpath("//div[contains(text(), ' cannot be empty')]")
                         .exists());
         subjectRepository.delete(subject);
     }
@@ -125,12 +133,12 @@ class SubjectControllerIntegrationTest {
         subject.setTitle("math12");
         subjectRepository.save(subject);
         mockMvc
-                .perform(MockMvcRequestBuilders.post("/subject/{id}/edit", subject.getId())
+                .perform(MockMvcRequestBuilders.post("/subject/{id}/edit", 1)
                         .param("title", "math22"))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/subjects"))
                 .andDo(
                         result -> MockMvcResultMatchers
-                                .xpath("//table//tbody//tr[0]//td[1]//a[contains(title(), 'math22')]")
+                                .xpath("//div//div//table//tbody//tr[0]//td[1]//a[contains(title(), 'math22')]")
                                 .exists()
 
        );
