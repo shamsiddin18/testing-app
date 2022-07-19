950519f31c47b7edc4b807ba6b6292318cfd3502
Shamsiddin <shamsiddin042@mail.ru>
2022-07-19 13:42:27 +0500

    fix user test

--- src/test/java/com/testapp/user/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/testapp/user/controller/UserControllerIntegrationTest.java
@@ -42,12 +42,13 @@ public class UserControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-//    @Test
-//    public void when_user_is_not_authenticated_register_page_should_redirect_to_login() throws Exception{
-//        mockMvc
-//               .perform(MockMvcRequestBuilders.get("/register"))
-//               .andExpect(MockMvcResultMatchers.redirectedUrl("/register"));
-//    }
+    @Test
+    @WithMockUser(username = "test", password = "test")
+    public void when_user_is_authenticated_register_page_should_redirect_to_home_page() throws Exception{
+        mockMvc
+               .perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 
     @Test
     @WithMockUser(username = "test", password = "test")
